2ca7974cace720808b72d0f9b57b8f355eff464c
nurlan007 <nurlan@artifactlabs.io>
2022-05-22 13:41:24 +0400

    webm to mp4 local video

--- src/components/sections/SectionFirst.vue
+++ src/components/sections/SectionFirst.vue
@@ -67,8 +67,8 @@
           poster="@/assets/video/posters/01.png"
 
         >
-          <source src="https://drive.google.com/uc?export=download&id=1ChJjVsgyUlT8GZeVVgbb9TIKIr6CereT" type="video/mp4" />
-          <source src="@/assets/video/webm/01.webm" type="video/webm" />
+          <source src="https://drive.google.com/uc?export=download&id=1ChJjVsgyUlT8GZeVVgbb9TIKIr6CereT" type="video/webm" />
+          <source src="@/assets/video/mp4/01.mp4" type="video/mp4" />
         </video>
       </div>
     </div>
