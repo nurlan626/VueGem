a2dd0a4c408aa270114b71ff5a8a41dec95ae11a
nurlan007 <nurlan@artifactlabs.io>
2022-05-22 13:52:57 +0400

    add mp4 video folder

--- src/components/sections/Header.vue
+++ src/components/sections/Header.vue
@@ -12,7 +12,7 @@
         </div>
       </router-link>
       <div class="header__links">
-        <div class="header__links-text">Join Community 13:41</div>
+        <div class="header__links-text">Join Community 13:52</div>
         <div class="header__links-icons">
           <a class="header__logo-a" href="#">
             <img
